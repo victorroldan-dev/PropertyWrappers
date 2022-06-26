0323930818f637639203c85505569794cb2cb311
Victor <victorroldan.dev@gmail.com>
2022-06-26 11:25:08 -0500

    Initial Commit

---
+++ PropertyWrappers/PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersApp.swift
+//  PropertyWrappers
+//
+//  Created by Victor Roldan on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
